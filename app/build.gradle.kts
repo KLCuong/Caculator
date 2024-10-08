68c0a1678af586585c19f4b06841a9d4122bf15a
KLCuong <Cuong.KL215004@sis.hust.edu.vn>
2024-10-08 22:10:55 +0700

    handler =.=

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
